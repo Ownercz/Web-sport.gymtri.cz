00868912007dd7f9132d4e2eb7b016f4c0fdad94
Radim Lipovčan <radim@lipovcan.cz>
2014-07-07 14:49:50 +0200

    Initial commit

---
+++ login_script.php
@@ -0,0 +1,7 @@
+﻿<?
+include "config.php";
+$login = $POST_["login"];
+$password = $POST_["password"];
+
+$login = $mysqli->real_escape_string($login);
+$password = $mysqli->real_escape_string($password);
