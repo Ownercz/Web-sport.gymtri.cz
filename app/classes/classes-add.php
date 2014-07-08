52b7224b6c6bcbc6ba1b636873ffc1a0cace015c
Radim Lipovčan <radim@lipovcan.cz>
2014-07-08 22:03:41 +0200

    Getting serious

--- app/classes/classes-add.php
+++ app/classes/classes-add.php
@@ -10,6 +10,15 @@
       </div>
       
       <?PHP
+      if(isset($_GET['id'])){$id= $_GET['id'];}else{}
+      if(isset($_GET['classes'])){$classes= $_GET['classes'];}else{}
+      if(isset($_GET['yearbegin'])){$yearbegin= $_GET['yearbegin'];}else{}
+      
+      if(isset($classes)){$classes = explode(",",$classes);}else{$classes = array();}
+      if(isset($_GET['classadd'])){array_push($classes,  $_GET['classadd']);$classes=implode(",",$classes);$a=12;}else{$classes=null;}
+      
+      if(isset($yearbegin)){$yearbegin = explode(",",$yearbegin);}else{$yearbegin = array();}
+      if(isset($_GET['yearadd'])){array_push($yearbegin,  $_GET['yearadd']);$yearbegin=implode(",",$yearbegin);$a=12;}else{$yearbegin=null;}
       include $_SERVER['DOCUMENT_ROOT']."/Web-sport.gymtri.cz/functions/dbconnect.php";
       if(isset($_GET['id'])){
       echo"<div class='alert alert-info' role='alert'>
@@ -17,16 +26,30 @@
       </div>";
       $request= "SELECT DISTINCT class,yearbegin FROM athletes ORDER by athletes.yearbegin DESC"  ; 
  $result = $mysqli->query($request);
-      echo"<h2><span class='label label-warning'>1. Vyberte soutěž</span></h2><form><div class='list-group'>";
+      echo"<h2><span class='label label-warning'>2. Vyberte třídy</span></h2><form><div class='list-group'>";
       while($row = $result->fetch_array(MYSQLI_NUM)){
       $beginyear = $row[1];
       $beginyear = preg_replace("/ - Name:.*/", "", $beginyear);
       $now = date("Y");
       $year = $now-$beginyear;
-      echo"<a href='?id=".$row[0]."' class='list-group-item'>Třída: <strong>".$year.".".$row[0]."</strong> Rok začátku:".$row[1]." </a>";}
+      
+      echo"<a href='?id=".$id."&classes=".$classes."&classadd=".$row[0]."&yearbegin=".$yearbegin."&yearadd=".$row[1]."' class='list-group-item'>Třída: <strong>".$year.".".$row[0]."</strong> Rok začátku:".$row[1]." </a>";}
       echo"</div></form>";
-      
-      
+      $i=1;
+      $tridy = array();
+      $zacatek = array();
+      $tridy =  explode(",",$classes);
+      $zacatek = explode(",",$yearbegin);
+      $count = count($tridy);
+      echo"<div class='alert alert-success' role='alert'><strong>Přidáno:</strong>   ";
+        
+      while($i<$count){
+      $now = date("Y");
+      $year = $now-$zacatek[$i];
+      print$year.".".$tridy[$i].",";
+      $i++;
+      }echo"<br> <strong>Každou třídu přidávejte pouze jednou! V případě chyby klepněte <a href='?id=".$id."'>sem</a>.</strong>
+      </div>";
       
       
       }
