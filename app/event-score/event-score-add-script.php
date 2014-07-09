5ac687ee947139a4b9672aca31396a224c1b1baa
Radim Lipovčan <radim@lipovcan.cz>
2014-07-09 20:35:49 +0200

    (ko)efficient

--- app/event-score/event-score-add-script.php
+++ app/event-score/event-score-add-script.php
@@ -1,1 +1,18 @@
-﻿
+﻿<?PHP
+
+function koeficient($vek,$sex){
+if($sex = "M"){
+if( in_array( $vek , range( 0 , 14 ) ) ){$koeficient = 1.06; return $koeficient;}
+if( in_array( $vek , range( 15 , 16 ) ) ){$koeficient = 1; return $koeficient;}
+if( in_array( $vek , range( 17 , 300 ) ) ){$koeficient = 0.97; return $koeficient;}
+}
+elseif($sex = "F"){
+if( in_array( $vek , range( 0 , 14 ) ) ){$koeficient = 1.07; return $koeficient;}
+if( in_array( $vek , range( 15 , 16 ) ) ){$koeficient = 1.05; return $koeficient;}
+if( in_array( $vek , range( 17 , 300 ) ) ){$koeficient = 1.02; return $koeficient;}
+}
+else{echo"Spatne zadane pohlavi."; exit;}
+}
+
+
+?>
