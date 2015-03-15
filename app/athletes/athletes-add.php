3de16e49325596b3d74c05e111c8fbfa0fe274ee
Ownercz <radim@lipovcan.cz>
2015-03-15 15:25:23 +0100

    Athletes to add

--- app/athletes/athletes-add.php
+++ app/athletes/athletes-add.php
@@ -10,7 +10,13 @@
       </div>
       <div class='row'>
         <div class='col-md-12'>
-          <table class='table'>
+            <div class='col-md-2'><strong>Třída</strong></div>
+            <div class='col-md-2'><strong>Jméno</strong></div>
+            <div class='col-md-2'><strong>Příjmení</strong></div>
+            <div class='col-md-2'><strong>Pohlaví</strong></div>
+            <div class='col-md-2'><strong>Datum narození</strong></div>
+            <div class='col-md-2'><strong>Začátek studia</strong></div><br>
+          <!--- <table class='table'>
             <thead>
               <tr>
                 <th>#</th>
@@ -22,17 +28,16 @@
                 <th>Začátek studia</th>
               </tr>
             </thead>
-            </table>
+            </table> --->
             
             <form name="formatus" action="athletes-add-script.php" method="POST">
             <div id="dynamicInput">
             <input type='text' class='form-control' name='trida1' placeholder='Třída "A"' value=''><input type='text' class='form-control' name='jmeno1' placeholder='Jméno "Pepa"'><input type='text' class='form-control' name='prijmeni1' placeholder='Příjmení "Novák"'><input type='text' class='form-control' name='sex1' placeholder='Muž "M" Žena "F"'><input type='text' class='form-control'name='narozeni1' placeholder='Tvar "dd-mm-yyyy"' ><input type='number' class='form-control' min='1900' max='2100' name='zacatek1' placeholder='"2008"' >
      
      </div>
+                <br>
      <input type="button" value="Přidat dalšího" onClick="addInput('dynamicInput');">
-<button type="submit" class="btn btn-primary" style="float:right">
-  <i class="icon-user icon-white"></i> Přidat atlety
-</button>
+<button type="submit" class="btn btn-primary" style="float:right"><i class="icon-user icon-white"></i> Přidat atlety </button>
     </form>
 
         </div>
@@ -71,7 +76,7 @@
      else {
            
           var newdiv = document.createElement('div');
-          newdiv.innerHTML = "<input type='text' class='form-control' name='trida" + (counter + 1) + "' placeholder='Třída' value=''><input type='text' class='form-control' name='jmeno" + (counter + 1) + "' placeholder='Jméno'><input type='text' class='form-control' name='prijmeni" + (counter + 1) + "' placeholder='Příjmení'><input type='text' class='form-control' name='sex" + (counter + 1) + "' placeholder='M nebo F'><input type='text' class='form-control' name='narozeni" + (counter + 1) + "' placeholder='dd-mm-yyyy' ><input type='number' class='form-control' min='1900' max='2100'name='zacatek" + (counter + 1) + "' >";
+          newdiv.innerHTML = "<input type='text' class='form-control' name='trida" + (counter + 1) + "' placeholder='Třída &quot;A&quot;' value=''><input type='text' class='form-control' name='jmeno" + (counter + 1) + "' placeholder='Jméno &quot;Pepa&quot;'><input type='text' class='form-control' name='prijmeni" + (counter + 1) + "' placeholder='Příjmení &quot;Novák&quot;'><input type='text' class='form-control' name='sex" + (counter + 1) + "' placeholder='Muž &quot;M&quot; Žena &quot;F&quot;'><input type='text' class='form-control' name='narozeni" + (counter + 1) + "' placeholder='Tvar &quot;dd-mm-yyyy&quot;' ><input type='number' class='form-control' min='1900' placeholder='&quot;2008&quot;'max='2100'name='zacatek" + (counter + 1) + "' >";
           document.getElementById(divName).appendChild(newdiv);
            document.getElementsByName("trida"+(counter + 1)).item(0).value=document.getElementsByName("trida"+(counter)).item(0).value;
            document.getElementsByName("zacatek"+(counter + 1)).item(0).value=document.getElementsByName("zacatek"+(counter)).item(0).value;
