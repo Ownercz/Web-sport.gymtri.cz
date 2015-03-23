1578e2abdb71976cca5d4411ce336e91404ebc01
Radim Lipovčan <radim@lipovcan.cz>
2015-03-23 11:14:26 +0100

    Athlete info

--- functions/functions.php
+++ functions/functions.php
@@ -12,3 +12,21 @@ function athleteinfo($athleteid,$mysqli){
     $row = $result->fetch_array(MYSQLI_ASSOC);
     return $row;
 }
+function eventdiscipline($disciplineid,$eventid,$mysqli){
+    $request = "SELECT * FROM `event_score` WHERE `event_id` = $eventid AND `discipline_id` = $disciplineid";
+    $result = $mysqli->query($request);
+    $row = $result->fetch_array(MYSQLI_ASSOC);
+    return $row;
+}
+function removeevent($eventid,$mysqli){
+    $request = "DELETE FROM `sport_gymtri_cz`.`event_score` WHERE `event_score`.`event_id` = $eventid";
+    $result = $mysqli->query($request);
+    $request = "DELETE FROM `sport_gymtri_cz`.`event` WHERE `event`.`id` = $eventid";
+    $result = $mysqli->query($request);
+    return $result;
+}
+function removescore($eventid,$athleteid,$mysqli){
+    $request = "DELETE FROM `sport_gymtri_cz`.`event_score` WHERE `event_score`.`event_id` = $eventid AND `event_score`.`athlete_id` = $athleteid ";
+    $result = $mysqli->query($request);
+    return $result;
+}
