2d6c0c441fa92de9f6359d0ffc53b055733948f0
Radim Lipovčan <radim@lipovcan.cz>
2015-03-24 11:59:54 +0100

    Login system

--- public/login.php
+++ public/login.php
@@ -4,4 +4,12 @@
  * User: radim_000
  * Date: 24. 3. 2015
  * Time: 11:54
- */ 
+ */
+
+
+
+/*
+ * ON-SUCCESS
+ * ON-FAIL
+ * LOG DO MYSQL
+ */
--- public/todo.php
+++ public/todo.php
@@ -4,4 +4,16 @@
  * User: radim_000
  * Date: 24. 3. 2015
  * Time: 11:51
- */ 
+ */
+
+/*
+ * System pro import jednotlivych atletu do systemu
+ * Samostatne oddelene od hlavni appky, ktera nebude pristupna pres net
+ * Moznosti:
+ * - login
+ * - vlozeni zavodniku + jejich discipliny
+ * - export danych dat
+ * - casove otevrene prihlaseni 2 tydny pred zacatkem souteze (rucne se nastavi)
+ * - casove zavreni prihlaseni 2 dny pred zacatkem souteze
+ * - moznost upravy startovek az na miste
+ */
