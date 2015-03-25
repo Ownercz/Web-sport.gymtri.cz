9a9ad98e7d63e6fe0a3087a6ea50e05b636e97fa
Radim Lipovčan <radim@lipovcan.cz>
2015-03-25 23:49:02 +0100

    Sometimes I even amaze myself

--- app/athletes/athletes-edit-class-script.php
+++ app/athletes/athletes-edit-class-script.php
@@ -23,7 +23,7 @@ if (isset($_GET['delete'])) {
     echo "<script>window.close();</script>";
     exit;
 }
-if(isset($_POST['jmeno'])==''){}else{
+if($_POST['jmeno']==''){}else{
 $request = "UPDATE `sport_gymtri_cz`.`athletes` SET `first_name` = '$jmeno', `last_name` = '$prijmeni', `gender` = '$sex', `birthdate` = '$narozeni', `class` = '$trida', `yearbegin` = '$zacatek' WHERE `athletes`.`id` = $id;";
 $result = $mysqli->query($request);}
 
