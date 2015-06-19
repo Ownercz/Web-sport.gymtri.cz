ad11e978e4a0825b3f287a6bb1bdec5753c8da14
Radim Lipovčan <radim@lipovcan.cz>
2015-06-20 00:04:21 +0200

    Night fix

--- app/event-print/event-score-add.php
+++ app/event-print/event-score-add.php
@@ -4,111 +4,8 @@
 <meta charset="UTF-8"/>
 <head>
     <title></title>
-    <style type="text/css">
-        body {
-            margin: 0;
-            padding: 0;
-            background-color: #FAFAFA;
-            font: 12pt "Tahoma";
-        }
-
-        h1 {
-            font-size: 15pt;
-            text-align: center;
-
-        }
-
-        h2 {
-            font-size: 11pt;
-            text-align: center;
-            border-bottom: 1px solid black;
-
-        }
-
-        table {
-            color: #333;
-            font-family: Helvetica, Arial, sans-serif;
-            width: 640px;
-            border-collapse: collapse;
-            border-spacing: 0;
-        }
-
-        td, th {
-            border: 1px solid transparent; /* No more visible border */
-            height: 30px;
-            transition: all 0.3s; /* Simple transition for hover effect */
-        }
-
-        th {
-            background: #DFDFDF; /* Darken header a bit */
-            font-weight: bold;
-        }
-
-        td {
-            background: #FAFAFA;
-            text-align: center;
-        }
-
-        /* Cells in even rows (2,4,6...) are one color */
-        tr:nth-child(even) td {
-            background: #F1F1F1;
-        }
-
-        /* Cells in odd rows (1,3,5...) are another (excludes header cells)  */
-        tr:nth-child(odd) td {
-            background: #FEFEFE;
-        }
-
-        tr td:hover {
-            background: #666;
-            color: #FFF;
-        }
-
-        /* Hover cell effect! */
-
-        * {
-            box-sizing: border-box;
-            -moz-box-sizing: border-box;
-        }
-
-        .page {
-            width: 21cm;
-            min-height: 29.7cm;
-            padding: 2cm;
-            margin: 1cm auto;
-            border: 1px #D3D3D3 solid;
-            border-radius: 5px;
-            background: white;
-            box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-        }
-
-        .subpage {
-            padding: 0, 5cm;
-
-            height: 267mm;
-
-        }
-
-        @page {
-            size: A4;
-            margin: 0;
-        }
-
-        @media print {
-            .page {
-                margin: 0;
-                border: initial;
-                border-radius: initial;
-                width: initial;
-                min-height: initial;
-                box-shadow: initial;
-                background: initial;
-                page-break-after: always;
-            }
-        }
-
-
-    </style>
+    <link href='print.css' rel='stylesheet'>
+    <link href='nice.css' rel='stylesheet'>
 </head>
 <body>
 
@@ -160,7 +57,7 @@ if (isset($_GET['id'])) {
 
     $body = 0;
 
-    echo "<div class='book'>
+    echo "<page size='A4'><div class='book'>
     <div class='page'>
         <div class='subpage'><h1>Gymtri výsledková listina - <strong>" . $row2[1] . "</strong></h1>
         Soutěž konána dne: " . $row2[4] . " <br>Vytvořil: " . $row2[2] . " <br>Zapisovali: " . $row2[3] . " <br> <h2>Třída: " . $trida . "</h2>";
@@ -205,9 +102,10 @@ echo " <h2>Formát výkonů</h2>
      <tr><td>25m,50m,100m plavání</td><td>sekundy</td><td>[s]</td></tr>
 
      </table>
-     
+<div style='font-size:10px; position:relative;bottom:0;'><div style='float:right;'>
+Gymtri Sport verze " . $version . "<br>© Radim Lipovčan 2015</div>
     </div></div>
-</div>";
+</div></page>";
 
 
 ?>
--- app/event-score/event-score-add-script.php
+++ app/event-score/event-score-add-script.php
@@ -45,51 +45,63 @@ $koeficient = koeficient($vek, $sex);
 //echo $koeficient."|".$vykon."|".$sex."<br";
 if ($discipline == "60m") {
     $points = sedesatka($koeficient, $vykon, $sex, $mysqli);
+	if($points==$koeficient){$points=0;}
     $request = "UPDATE `sport_gymtri_cz`.`event_score` SET `discipline_id` = '1', `score_value` = '$vykon', `score_points` = '$points', `koeficient` = '$koeficient' WHERE `event_score`.`id` = $id";
     echo $request;
     $result = $mysqli->query($request);
 } elseif ($discipline == "100m") {
     $points = stovka($koeficient, $vykon, $sex, $mysqli);
+	if($points==$koeficient){$points=0;}
     $request = "UPDATE `sport_gymtri_cz`.`event_score` SET `discipline_id` = '2', `score_value` = '$vykon', `score_points` = '$points', `koeficient` = '$koeficient' WHERE `event_score`.`id` = $id";
     $result = $mysqli->query($request);
 } elseif ($discipline == "1500m") {
     $points = patnactistovka($koeficient, $vykon, $sex, $mysqli);
+	if($points==$koeficient){$points=0;}
     $request = "UPDATE `sport_gymtri_cz`.`event_score` SET `discipline_id` = '3', `score_value` = '$vykon', `score_points` = '$points', `koeficient` = '$koeficient' WHERE `event_score`.`id` = $id";
     $result = $mysqli->query($request);
 } elseif ($discipline == "3000m") {
     $points = tritisicovka($koeficient, $vykon, $sex, $mysqli);
+	if($points==$koeficient){$points=0;}
     $request = "UPDATE `sport_gymtri_cz`.`event_score` SET `discipline_id` = '4', `score_value` = '$vykon', `score_points` = '$points', `koeficient` = '$koeficient' WHERE `event_score`.`id` = $id";
     $result = $mysqli->query($request);
 } elseif ($discipline == "dálka") {
     $points = dalka($koeficient, $vykon, $sex, $mysqli);
+	if($points==$koeficient){$points=0;}
     $request = "UPDATE `sport_gymtri_cz`.`event_score` SET `discipline_id` = '5', `score_value` = '$vykon', `score_points` = '$points', `koeficient` = '$koeficient' WHERE `event_score`.`id` = $id";
     $result = $mysqli->query($request);
 } elseif ($discipline == "výška") {
     $points = vyska($koeficient, $vykon, $sex, $mysqli);
+	if($points==$koeficient){$points=0;}
     $request = "UPDATE `sport_gymtri_cz`.`event_score` SET `discipline_id` = '6', `score_value` = '$vykon', `score_points` = '$points', `koeficient` = '$koeficient' WHERE `event_score`.`id` = $id";
     $result = $mysqli->query($request);
 } elseif ($discipline == "míč") {
     $points = mic($koeficient, $vykon, $sex, $mysqli);
+	if($points==$koeficient){$points=0;}
     $request = "UPDATE `sport_gymtri_cz`.`event_score` SET `discipline_id` = '7', `score_value` = '$vykon', `score_points` = '$points', `koeficient` = '$koeficient' WHERE `event_score`.`id` = $id";
     $result = $mysqli->query($request);
 } elseif ($discipline == "granát") {
     $points = granat($koeficient, $vykon, $sex, $mysqli);
+	if($points==$koeficient){$points=0;}
     $request = "UPDATE `sport_gymtri_cz`.`event_score` SET `discipline_id` = '8', `score_value` = '$vykon', `score_points` = '$points', `koeficient` = '$koeficient' WHERE `event_score`.`id` = $id";
     $result = $mysqli->query($request);
 } elseif ($discipline == "šplh") {
     $points = splh($koeficient, $vykon, $sex, $mysqli);
+	if($points==$koeficient){$points=0;}
     $request = "UPDATE `sport_gymtri_cz`.`event_score` SET `discipline_id` = '9', `score_value` = '$vykon', `score_points` = '$points', `koeficient` = '$koeficient' WHERE `event_score`.`id` = $id";
     $result = $mysqli->query($request);
 } elseif ($discipline == "25m plavání") {
     $points = dvacetpetkavz($koeficient, $vykon, $sex, $mysqli);
+	if($points==$koeficient){$points=0;}
     $request = "UPDATE `sport_gymtri_cz`.`event_score` SET `discipline_id` = '10', `score_value` = '$vykon', `score_points` = '$points', `koeficient` = '$koeficient' WHERE `event_score`.`id` = $id";
     $result = $mysqli->query($request);
 } elseif ($discipline == "50m plavání") {
     $points = padesatkavz($koeficient, $vykon, $sex, $mysqli);
+	if($points==$koeficient){$points=0;}
     $request = "UPDATE `sport_gymtri_cz`.`event_score` SET `discipline_id` = '11', `score_value` = '$vykon', `score_points` = '$points', `koeficient` = '$koeficient' WHERE `event_score`.`id` = $id";
     $result = $mysqli->query($request);
 } elseif ($discipline == "100m plavání") {
     $points = stovkavz($koeficient, $vykon, $sex, $mysqli);
+	if($points==$koeficient){$points=0;}
     $request = "UPDATE `sport_gymtri_cz`.`event_score` SET `discipline_id` = '12', `score_value` = '$vykon', `score_points` = '$points', `koeficient` = '$koeficient' WHERE `event_score`.`id` = $id";
     $result = $mysqli->query($request);
 } else {
