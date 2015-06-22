0229b9da51f5d5aca5e18f599d547924a9843591
Radim Lipovčan <radim@lipovcan.cz>
2015-06-22 17:51:27 +0200

    Brothers and sisters combinations finder

---
+++ app/brotherAndSisters/find.php
@@ -0,0 +1,50 @@
+<?PHP include"../../functions/check.php";
+include"../../functions/functions.php";
+
+/**
+ * Created by PhpStorm.
+ * User: radim_000
+ * Date: 22. 6. 2015
+ * Time: 17:26
+ */
+include"../../functions/dbconnect.php";
+$request = "SELECT * FROM  `event_score` WHERE  `event_id` =1";
+$i=0;
+$lastname=array();
+$id=array();
+$true=array();
+$basName=array();
+$result = $mysqli->query($request);
+while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+    $lastname[$i]=$row["last_name"];
+    $firstname[$i]=$row["first_name"];
+    $id[$i]=$row["id"];
+    $i++;
+}
+$x=0;
+$y=0;
+while($x<$i){
+    //echo substr($lastname[$x], 0, 5)."<br>";
+    $z=0;
+    while($z<$i){
+        if(substr($lastname[$z], 0, 5)==substr($lastname[$x], 0, 5) && $z!=$x && $firstname[$z]!=$firstname[$x]){
+            $true[$y]=$id[$z];//echo$lastname[$z];
+            $basName[$y]=$lastname[$z];
+            $y++;
+        }
+        $z++;
+    }
+
+    $x++;
+}
+echo"<h1>HIGHLY EXPERIMENTAL</h1><br>";
+echo"Najít možné sourozence<br>";
+foreach($true as $value){
+    $request = "SELECT *    FROM  `event_score`WHERE  `id` =$value;";
+    $result = $mysqli->query($request);
+while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+    echo $row["first_name"].$row["last_name"].$row["class_name"]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>";
+
+
+}
+}
