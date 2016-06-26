53544de087b72dbbaffa3f7b0f514b7640b2fc74
Radim Lipovčan <radim@lipovcan.cz>
2016-06-26 21:38:32 +0200

    Handy import fix

--- db-points/importdata.php
+++ db-points/importdata.php
@@ -1,39 +1,65 @@
-<?PHP
+﻿<?PHP
 /*
  * Import bodů do MySQL databáze
  * Určeno pouze pro přidávání, změnu údajů jednotlivých bodových ohodnocení disciplín.
  * V případě přidání disciplíny je potřeba také zanést do tabulky disciplines novou položku.
  */
-include "config.php";
+include "../functions/config.php";
+include "../functions/dbconnect.php";
 //$mysqli->query("TRUNCATE points");
-$file = @fopen('LIP_RAD_SEZNAM.CSV', "r") ;
+$file = @fopen('LIP_RAD_SEZNAM.csv', "r") ;
 $i=0;
+echo "<br>";
 while (!feof($file))
 {
-    if($i=0){break;}$i++;
+
     $line = fgets($file) ;
-    $line = str_replace(";;",";",$line);
-    echo $line."<br>";
     $line = explode(';',$line) ;
-    $i=0;
-    while($i<21){
-        $line[$i] = str_replace(",",".",$line[$i]);
-        if((substr_count($line[$i], ':')==1)){$timing=explode(':',$line[$i]) ; $timing[0]=$timing[0]*60;$time=$timing[0]+$timing[1]; $line[$i] = $time;}
-        $i++;
-    }
     $jmeno= $line[0];
     $prijmeni=$line[1];
     $pohlavi= $line[6];
-	if($pohlavi=="Z"){$pohlavi="F";}else{$pohlavi="M";}
+    $pohlavi=substr($pohlavi,0,1);
     $datumnarozeni=$line[3];
+    $datumnarozeni= str_replace(".", "-", $datumnarozeni);
     $trida= $line[2];
-	$trida=substr($trida,-1);
+    $trida=substr($trida,-1);
     $zacatekskoly=$line[5];
-	$zacatekskoly=substr($zacatekskoly,-4);
-    $request= "INSERT INTO `sport_gymtri_cz`.`athletes` (`id`, `first_name`, `second_name`, `gender`, `birthdate`, `class`, `yearbegin`) VALUES (NULL, '$jmeno', '$prijmeni', '$pohlavi', '$datumnarozeni', '$trida', '$zacatekskoly');"; 
-	echo $request;
-	//$result = $mysqli->query($request);
-}   
+    $zacatekskoly=substr($zacatekskoly,-4);
+
+
+    if($pohlavi=="Z"){$pohlavi="F";}
+    elseif($pohlavi=="M"){$pohlavi="M";}
+    else{
+        $jmeno= $line[0]." ".$line[1];
+
+        $prijmeni=$line[2];
+        $pohlavi= $line[7];
+        $pohlavi=substr($pohlavi,0,1);
+        $datumnarozeni=$line[4];
+        $trida= $line[3];
+        $trida=substr($trida,-1);
+        $zacatekskoly=$line[6];
+        $zacatekskoly=substr($zacatekskoly,-4);
+    }
+
+    $request = "SELECT * FROM `sport_gymtri_cz`.`athletes`  WHERE first_name LIKE '$jmeno' AND last_name LIKE '$prijmeni' AND yearbegin = $zacatekskoly;";
+    //  echo $request;
+    $result = $mysqli->query($request);
+    $row_cnt = $result->num_rows;
+
+    printf("Result set has %d rows.\n", $row_cnt);
+    if ($row_cnt==0) { $request= "INSERT INTO `sport_gymtri_cz`.`athletes` (`id`, `first_name`, `last_name`, `gender`, `birthdate`, `class`, `yearbegin`) VALUES (NULL, '$jmeno', '$prijmeni', '$pohlavi', '$datumnarozeni', '$trida', '$zacatekskoly');";
+        echo $request;
+        $result = $mysqli->query($request);
+    }else{
+        $request="UPDATE `sport_gymtri_cz`.`athletes` SET `gender`='$pohlavi', `birthdate`='$datumnarozeni'  WHERE  `first_name`= '$jmeno' AND  `last_name`= '$prijmeni' ;";
+        $result = $mysqli->query($request);
+        echo $request;
+    }
+
+}
+
+
 
 fclose($file) ;//$mysqli->query("TRUNCATE points");
 ?>
